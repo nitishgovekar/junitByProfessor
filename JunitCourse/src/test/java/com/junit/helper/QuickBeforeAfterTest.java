f566f10f70a2e942c664d4f323a54297a4706170
nitish.govekar <nitishgovekar1992@gmail.com>
2021-10-16 10:12:10 +0530

    Updated code to implement dummy examples of @BeforeClass @AfterClass

--- JunitCourse/src/test/java/com/junit/helper/QuickBeforeAfterTest.java
+++ JunitCourse/src/test/java/com/junit/helper/QuickBeforeAfterTest.java
@@ -1,11 +1,20 @@
 package com.junit.helper;
 
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class QuickBeforeAfterTest {
 
+	@BeforeClass
+	public static void beforeClass() {
+		//We can only intialize class level things in @BeforeClasss
+		System.out.println("Before Class");
+	}
+	
+	
 	@Before
 	public void setup() {
 		System.out.println("Before runs before every test method");
@@ -25,6 +34,11 @@ public class QuickBeforeAfterTest {
 	@After
 	public void tearDown() {
 		System.out.println("After runs after every test method");
+	}
+	
+	@AfterClass
+	public static void afterClass() {
+		System.out.println("After Class");
 	}
 
 }
