0d8915c2d3a394f6abef55ac8d0bf6b9dcd99fd9
nitish.govekar <nitishgovekar1992@gmail.com>
2021-10-16 10:33:20 +0530

    Updated code to compare Arrays in Junit

---
+++ JunitCourse/src/test/java/com/junit/helper/ArraysCompareTest.java
@@ -0,0 +1,19 @@
+package com.junit.helper;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+public class ArraysCompareTest {
+
+	@Test
+	public void testAssertArrayEquals() {
+
+	int [] numbers = {12,10,1,2};
+	int [] expectedValue = {1,2,10,12};
+	Arrays.sort(numbers);
+	assertArrayEquals(expectedValue, numbers);
+	}
+}
