0b4657500515faf3c408d131bf040ee5538fc8e7
nitish.govekar <nitishgovekar1992@gmail.com>
2021-10-16 09:06:31 +0530

    Refactored code to give appropriate naming conventions to the methods

--- JunitCourse/src/test/java/com/junit/helper/StringHelperTest.java
+++ JunitCourse/src/test/java/com/junit/helper/StringHelperTest.java
@@ -5,17 +5,19 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class StringHelperTest {
+	StringHelper helper = new StringHelper();
 
 	@Test
-	public void test() {
-		
-		StringHelper stringHelper = new StringHelper();
-		assertEquals("CD", stringHelper.truncateAInFirst2Positions("AACD"));
-		assertEquals("CD", stringHelper.truncateAInFirst2Positions("ACD"));
-	
-	//assertEquals("ABC", "ABCD");
-	//org.junit.ComparisonFailure: expected:<ABC[]> but was:<ABC[D]>
-	
+	public void testTruncateAInFirst2Positions() {
+		assertEquals("CD", helper.truncateAInFirst2Positions("AACD"));
 	}
 
+	@Test
+	public void testTruncateAInFirstPosition() {
+		assertEquals("CD", helper.truncateAInFirst2Positions("ACD"));
+	}
 }
+/*
+  assertEquals("ABC", "ABCD");
+   org.junit.ComparisonFailure: expected:<ABC[]>but was:<ABC[D]>
+ */
